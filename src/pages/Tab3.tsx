a215933628ac8f24ab7a1ddff6a8b6d960e12c7c
paulichdom <paulic.domagoj@gmail.com>
2025-01-17 08:39:00 +0100

    implement dark mode

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonTitle,
+  IonToggle,
+  IonToolbar,
+  ToggleCustomEvent,
+} from "@ionic/react";
+import "./Tab3.css";
+import { useEffect, useState } from "react";
 
 const Tab3: React.FC = () => {
+  const [paletteToggle, setPaletteToggle] = useState(false);
+
+  // Listen for the toggle check/uncheck to toggle the dark palette
+  const toggleChange = (event: ToggleCustomEvent) => {
+    toggleDarkPalette(event.detail.checked);
+  };
+
+  // Add or remove the "ion-palette-dark" class on the html element
+  const toggleDarkPalette = (shouldAdd: boolean) => {
+    document.documentElement.classList.toggle("ion-palette-dark", shouldAdd);
+  };
+
+  // Check/uncheck the toggle and update the palette based on isDark
+  const initializeDarkPalette = (isDark: boolean) => {
+    setPaletteToggle(isDark);
+    toggleDarkPalette(isDark);
+  };
+
+  useEffect(() => {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    initializeDarkPalette(prefersDark.matches);
+
+    const setDarkPaletteFromMediaQuery = (mediaQuery: MediaQueryListEvent) => {
+      initializeDarkPalette(mediaQuery.matches);
+    };
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener("change", setDarkPaletteFromMediaQuery);
+
+    return () => {
+      prefersDark.removeEventListener("change", setDarkPaletteFromMediaQuery);
+    };
+  }, []);
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +59,18 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonListHeader>Appearance</IonListHeader>
+        <IonList inset={true}>
+          <IonItem>
+            <IonToggle
+              checked={paletteToggle}
+              onIonChange={toggleChange}
+              justify="space-between"
+            >
+              Dark Mode
+            </IonToggle>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
